67857efa8fd431fece2e29dd456742b3acf29321
Patricia Nykaza <pnykaza@calstatela.edu>
2018-10-26 21:39:54 +0000

    automatically backed up by learn

--- db/migrate/20181026213731_add_active_to_students.rb
+++ db/migrate/20181026213731_add_active_to_students.rb
@@ -1,7 +1,5 @@
 class AddActiveToStudents < ActiveRecord::Migration
   def change
-    add_column :students, :active, :boolean,
-    add_column :students, :default, :string
-    add_column :students, :, :false
+    add_column :students, :active, :boolean, default: false
   end
 end
